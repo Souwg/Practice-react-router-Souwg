9c5e0065f1fd1f473286fe3bedb5de89b3a77cc4
Souwg <sansanvzla@gmail.com>
2024-10-11 04:00:13 +0000

    cambios del dflex en el navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,10 +6,10 @@ export const Navbar = () => {
 	return (
 	<div className="container-fluid" style={{ background: '#eee' }}>
 		<div className="container p-1">
-		<nav className="navbar mb-3 py-0 mt-2">
-			<Link to="/"><img src={ Logo } style={{ width: '30%' }} alt="Starwars"/></Link>
+		<nav className="navbar d-flex justify-content-between  mb-3 py-0 mt-2">
+			<Link to="/"><img src={ Logo } style={{ width: '6rem' }} alt="Starwars"/></Link>
 			<div className="ml-auto">
-				<Link to="/demo">
+				<Link to="/">
 					<button className="btn btn-primary me-2">favorites</button>
 				</Link>
 			</div>
